2da500abf7246d467c297401dcd0280314460ff7
Mateus Brum <mateushenriquebrum@gmail.com>
2026-05-09 23:00:51 +0100

    comments

--- lib/raccoon.ex
+++ lib/raccoon.ex
@@ -5,11 +5,17 @@ defmodule Raccoon do
 
   def normalize(row) do
     row
+    # upper case everything
     |> Map.new(fn {k, v} -> {k, String.upcase(v)} end)
+    # remove R$ currency
     |> Map.new(fn {k, v} -> {k, Regex.replace(~r/R\$/i, v, "")} end)
+    # remove € currency
     |> Map.new(fn {k, v} -> {k, Regex.replace(~r/€/i, v, "")} end)
+    # remove $ currency
     |> Map.new(fn {k, v} -> {k, Regex.replace(~r/\$/i, v, "")} end)
+    # float point with 2 digits
     |> Map.new(fn {k, v} -> {k, Regex.replace(~r/\.([0-9])$/i, v, ".\\g{1}0")} end)
+    # float point with 2 digits
     |> Map.new(fn {k, v} -> {k, Regex.replace(~r/,([0-9])$/i, v, ",\\g{1}0")} end)
   end
 end
